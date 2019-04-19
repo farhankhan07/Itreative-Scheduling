d2d5c408916ad22a88486089353ad962f366115a
farhankhan07 <49692741+farhankhan07@users.noreply.github.com>
2019-04-19 18:51:42 +0530

    bug fixes

--- Scedhuler.c
+++ Scedhuler.c
@@ -1,74 +1,67 @@
-#include<pthread.h>
 #include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-
-#define NEW 0
-#define READY 1
-#define WAITING 2
-#define RUNNING 3
-#define EXIT 4
-
-int currenttime=0,runningtime=0,ALLEXIT=0,TimeQuantum,CT=1,n;
-struct Proc 
+int main()
 {
-	int pid;
-	int state;
-	int timeleft;
-	int at;
-        int ae;
-	int wt,tat,ct,exect;
-	struct Proc *prev;
-	struct Proc *next;
-} *pa;
-struct Queue
-{
-	struct Proc *front ,*rear;
-}*ReadyQueue;
+	int n,i,count,t,flag=0;
+	printf("\n Enter Number of processes ::");
+	scanf("%d",&n);
+	int exect[n],Arrival_Time[n],TimeQuantum=3,Proc[n],tat=0,wt=0,rt[n],r=n;
+	for(i=0;i<n;i++)
+	
+	{
+		
+		Proc[i]=i;
+		printf("\nEnter Arrival Time of Process %d :: ",i+1);
+		scanf("%d",&Arrival_Time[i]);
+		printf("Enter Burst Time of Process %d ::",i+1);
+		scanf("%d",&exect[i]);
+		rt[i]=exect[i];
+	}
+	
+	printf("\nProcess\t Turnaround Time Waiting Time\n");
+	for(t=0,count=0;r!=0;)
+	
+	{
+		
+		if(rt[count]<=TimeQuantum&&rt[count]>0)
+		
+		{
+			
+			t=t+rt[count];
+			rt[count]=0;
+			flag=1;
+			
+		}
+		
+		else if(rt[count]>0)
+		
+		{
+			
+		rt[count]-=TimeQuantum;
+		t=t+TimeQuantum;
+		
+		}
+		
+		if(rt[count]==0&&flag==1)
+		
+		{
+			
+		r--;
+		printf("P[%d]\t|\t%d\t|\t%d\n",count+1,t-Arrival_Time[count],t-Arrival_Time[count]-exect[count]);
+		wt+=t-Arrival_Time[count]-exect[count];
+		tat+=t-Arrival_Time[count];
+		flag=0;
+		
+		}
+		
+		if(count==n-1)
+		count=0;
+		else if(Arrival_Time[count+1]<=t)
+		count++;
+		else
+		count=0;
+	}
+	
+printf("\nAverage WT= %f\n",wt*1.0/n);
+printf("Avg TAT = %f",tat*1.0/n);
 
-void SortTimeleft()
-{
-    for(int i=1;i<n;i++)
-    {
-        for(int j=0;j<n-i;j++)
-        {
-            if(pa[j].timeleft > pa[j+1].timeleft)
-             {
-                struct Proc temp = pa[j];
-               pa[j]=pa[j+1];
-                pa[j+1]=temp;
-            }
-        }
-    }
-
-}
-void enqueue(struct Proc *p)
-{
-	 if(ReadyQueue->front==NULL)
-      { 
-         p->prev=NULL;
-
-         p->next=NULL;
-         ReadyQueue->front=p;           
-         ReadyQueue->rear=p;
-         return;
-      }
-     p->prev=ReadyQueue->rear;
-     ReadyQueue->rear->next=p;
-     ReadyQueue->rear=p;
-}
-struct Proc *  deQueue()
-{
-     if(ReadyQueue->front==NULL)
-     {
-	     return NULL;
-     }
-     struct Proc * temp=ReadyQueue->front;
-     ReadyQueue->front=ReadyQueue->front->next;
-     temp->next=NULL;
-     if(ReadyQueue->front==NULL)
-     {
-	     ReadyQueue->rear=NULL;
-     }
-     return temp;
 }
